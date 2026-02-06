6a081c40f969cdd909a4fbf197ab6814e2a2d83e
Kwon <ksh03616@gmail.com>
2026-02-06 20:52:50 +0900

    [level 2] Title: 전화번호 목록, Time: 504.22 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,6 +1,12 @@
 def solution(phone_book):
-    phone_book.sort()
-    for i in range(len(phone_book)-1):
-        if phone_book[i + 1].startswith(phone_book[i]):
-            return False
-    return True
+    answer = True
+    hash_map = {}
+    for phone_number in phone_book: # 번호를 하나씩 꺼내서
+        hash_map[phone_number] = 1 # 딕셔너리 만들고 {"119": 1, "97674223": 1, "1195524421":1}
+    for phone_number in phone_book: # 번호를 하나씩 꺼내서
+        temp = "" # 변수 생성 및 초기화, 문자열
+        for number in phone_number: # 딕셔너리에서 키값 번호 하나씩 꺼내서
+            temp += number # 번호 하나씩 넣고
+            if temp in hash_map and temp != phone_number: # 변수가 접두어이고(들어가있고) 키값으로 있고 벼수가 키값이 아니면
+                answer = False
+    return answer
